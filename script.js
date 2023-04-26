9ba87d5d3e9271e8a96cbab055f39d899fd42906
SainaKey <sainakey5@gmail.com>
2023-04-26 15:33:35 +0900

    ポリゴン動くようになった

--- script.js
+++ script.js
@@ -7,15 +7,6 @@ onload = function(){
     // webglコンテキストを取得
     var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
     
-    // canvasを初期化する色を設定する
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    
-    // canvasを初期化する際の深度を設定する
-    gl.clearDepth(1.0);
-    
-    // canvasを初期化
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
     // 頂点シェーダとフラグメントシェーダの生成
     var v_shader = create_shader('vs');
     var f_shader = create_shader('fs');
@@ -64,40 +55,70 @@ onload = function(){
     var mMatrix = m.identity(m.create());
     var vMatrix = m.identity(m.create());
     var pMatrix = m.identity(m.create());
-    var mvpMatrix = m.identity(m.create());
     var tmpMatrix = m.identity(m.create());
+    var mvpMatrix = m.identity(m.create());
+
+    var uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
     
     // ビュー座標変換行列
     m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
-    
-    // プロジェクション座標変換行列
     m.perspective(90, c.width / c.height, 0.1, 100, pMatrix);
-
-    
-    // 各行列を掛け合わせ座標変換行列を完成させる
     m.multiply(pMatrix, vMatrix, tmpMatrix);
-    m.translate(mMatrix,[1.5,0.0,0.0],mMatrix);
-    m.multiply(tmpMatrix, mMatrix, mvpMatrix);
-    // uniformLocationの取得
-    var uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
-    // uniformLocationへ座標変換行列を登録
-    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-    // モデルの描画
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
 
-    //２つ目
-    mMatrix = m.identity(mMatrix);
-    m.translate(mMatrix,[-1.5,0.0,0.0],mMatrix);
-    m.multiply(tmpMatrix,mMatrix,mvpMatrix);
-    // uniformLocationの取得
-    var uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
-    // uniformLocationへ座標変換行列を登録
-    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-    // モデルの描画
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-    
-    // コンテキストの再描画
-    gl.flush();
+    var count = 0;
+
+    //恒常ループ
+    (function(){
+        //canvas初期化
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearDepth(1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        count++;
+
+        // カウンタを元にラジアンを算出
+        var rad = (count % 360) * Math.PI / 180;
+        
+        // モデル1は円の軌道を描き移動する
+        var x = Math.cos(rad);
+        var y = Math.sin(rad);
+        m.identity(mMatrix);
+        m.translate(mMatrix, [x, y + 1.0, 0.0], mMatrix);
+        
+        // モデル1の座標変換行列を完成させレンダリングする
+        m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+        gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+        
+        // モデル2はY軸を中心に回転する
+        m.identity(mMatrix);
+        m.translate(mMatrix, [1.0, -1.0, 0.0], mMatrix);
+        m.rotate(mMatrix, rad, [0, 1, 0], mMatrix);
+        
+        // モデル2の座標変換行列を完成させレンダリングする
+        m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+        gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+        
+        // モデル3は拡大縮小する
+        var s = Math.sin(rad) + 1.0;
+        m.identity(mMatrix);
+        m.translate(mMatrix, [-1.0, -1.0, 0.0], mMatrix);
+        m.scale(mMatrix, [s, s, 0.0], mMatrix)
+        
+        // モデル3の座標変換行列を完成させレンダリングする
+        m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+        gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+        
+        gl.flush();
+
+        // ループのために再帰呼び出し
+        //arguments.calleeで自分自身を参照できるらしい
+        setTimeout(arguments.callee, 1000 / 30);
+
+    })();
+
 
     // VBOをバインドし登録する関数
     function set_attribute(vbo, attL, attS){
